a78bd4fc5ee097640f3d6d67c16d9a9361d6e7f5
Chris Sutherland <csutherland@lamar.com>
2010-05-17 09:43:02 -0500

    show list of who voted for an issue

--- lib/issues_vote_hook.rb
+++ lib/issues_vote_hook.rb
@@ -6,9 +6,15 @@ class IssuesVoteHook < Redmine::Hook::ViewListener
     <%= content_tag('span', vv, :class => (vv > 0? 'votes-positive': ( vv < 0 ? 'votes-negative' : ''))) %>
     <% @project = @issue.project %>
     <% if authorize_for('issues', 'vote') && !@issue.voted_by_user? %>
-    <%= link_to("", {:controller => 'issues', :action => 'vote', :id => @issue, :vote => :up}, :class => 'icon icon-vote-up') %>
-    <%= link_to("", {:controller => 'issues', :action => 'vote', :id => @issue, :vote => :down}, :class => 'icon icon-vote-down') %>
+    <%= link_to('&nbsp;', {:controller => 'issues', :action => 'vote', :id => @issue, :vote => :up}, :class => 'icon icon-vote-up') %>
+    <%= link_to('&nbsp;', {:controller => 'issues', :action => 'vote', :id => @issue, :vote => :down}, :class => 'icon icon-vote-down') %>
     <% end %>
+    <% unless @issue.votes.empty? %>(<%= link_to_function('View', 'Effect.toggle("voter_list", "blind");') %>)<% end %>
+    <div id="voter_list" style="display:none">
+    <% @issue.votes.sort_by{|v| v.user.name}.each do |vote| -%>
+      <%= content_tag('span', vote.user.name, :class => (vote.vote ? 'votes-positive' : 'votes-negative')) %><br />
+    <% end -%>
+    </div>
     </td></tr>
 END
 end
