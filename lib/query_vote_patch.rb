a986bd77c905cc3cbd876be1656f2f836e8d76ff
Marek Hulan <mhulan@redhat.com>
2013-08-15 14:00:14 +0000

    More secure way of setting columns which works with recent redmine

--- lib/query_vote_patch.rb
+++ lib/query_vote_patch.rb
@@ -7,7 +7,7 @@ module QueryVotePatch
 
   def self.included(base)
     base.class_eval <<-END
-      @@available_columns += [QueryColumn.new(:votes_value, :sortable => "#{Issue.table_name}.votes_value")]
+      self.available_columns += [QueryColumn.new(:votes_value, :sortable => "#{Issue.table_name}.votes_value")]
 END
   end
 end
